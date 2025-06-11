73599fc6e06fc51ead7a4238d67afc6ce779c34c
cyberllloner <cyberllloner@proton.me>
2025-06-11 11:32:44 +0330

    Add new program

---
+++ c/printf-and-sleep.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    sleep(2);
+    printf("\nHello, my name is Hosein!\nI use gcc to complie.\n");
+    sleep(2);
+    printf("\nI think C programming language is cool.\nIt is fun to learn C!\n");
+    sleep(2);
+    return 0;
+}
